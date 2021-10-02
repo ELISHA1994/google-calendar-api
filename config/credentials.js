3e082e640e81a890ee7b4381943442856354d564
Elisha Bello <elishabello2014@gmail.com>
2021-10-02 17:11:12 +0100

    EE

--- config/credentials.js
+++ config/credentials.js
@@ -5,7 +5,7 @@ export default {
         "auth_uri":"https://accounts.google.com/o/oauth2/auth",
         "token_uri":"https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
-        "client_secret":"f_Nc6usr2ujagrbYyurJ20qs",
+        "client_secret":"",
         "redirect_uris":["http://localhost:3000","https://roja-tech-calendar-app-fronten.herokuapp.com"],
         "javascript_origins":["http://localhost:1337","http://localhost:3000","https://roja-tech-calendar-app-fronten.herokuapp.com"]
     }
